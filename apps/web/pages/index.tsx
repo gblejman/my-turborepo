105f606b575c6c774afcf991eceb2696118f782e
g <gblejman@gmail.com>
2022-05-07 13:42:55 -0300

    tlinktype

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,11 +1,18 @@
-import { Button, sample } from "ui";
+import { Button, sample, TLinkType } from "ui";
+
+const type: TLinkType = "address";
 
 export default function Web() {
   return (
     <div>
       <h1>Web</h1>
       <Button />
+
+      <h2>Sample</h2>
       <span>{JSON.stringify(sample)}</span>
+
+      <h2>type</h2>
+      <span>{type}</span>
     </div>
   );
 }
--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -1,5 +1,8 @@
 import * as React from "react";
 export * from "./Button";
 
+type TLinkType = "address" | "tx";
+export type { TLinkType };
+
 import sample from "./sample.json";
 export { sample };
